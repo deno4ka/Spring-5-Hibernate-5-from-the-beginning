ec7be701f938feda5e340fbd94c50d69052528f0
Denys Dvornyi <ddvornyi@geeksforless.net>
2019-05-29 19:17:51 +0300

    finished lesson #48 (not working... need fix)

--- spring-security-demo/src/main/java/com/den/springsecurity/config/SecurityConfig.java
+++ spring-security-demo/src/main/java/com/den/springsecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.den.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -30,6 +31,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.withUser("john").password(ENCODED_PASSWORD).roles("EMPLOYEE").and()
 				.withUser("mary").password(ENCODED_PASSWORD).roles("MANAGER").and()
 				.withUser("susan").password(ENCODED_PASSWORD).roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests() // restrict access based on the HttpServletRequest
+				.anyRequest().authenticated() // any request must be authenticated
+					.and()
+				.formLogin()
+					.loginPage("/showMyLogingPage") // specify request mapping
+					.loginProcessingUrl("/authenticateTheUser") // send data to this URL for processing
+					.defaultSuccessUrl("/", true)
+					.permitAll(); // everyone can see login page
+//		http.authorizeRequests()
+//				.anyRequest().authenticated()
+//				.and()
+//				.formLogin()
+//				.loginPage("/showMyLoginPage")
+//				.loginProcessingUrl("/authenticateTheUser")
+//				.permitAll();
 	}
 
 	@Bean
