76301903521ab3b7435516febe9376b53f867995
Denys Dvornyi <ddvornyi@geeksforless.net>
2019-06-14 16:58:28 +0300

    finished lesson #66 (started lesson #67 "Spring REST - CRUD Database Project - Update Customer")

--- spring-crm-rest-demo/src/main/java/com/deno4ka/spring/crm/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/deno4ka/spring/crm/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import com.deno4ka.spring.crm.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +30,15 @@ public class CustomerRestController {
 		if (customer == null) {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
+		return customer;
+	}
+
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		// also just in case the pass an id in JSON ... set id to 0
+		// this is force a save of new item ... instead of update
+		customer.setId(0); // if id is 0, then DAO will "INSERT" new customer
+		customerService.saveCustomer(customer);
 		return customer;
 	}
 
