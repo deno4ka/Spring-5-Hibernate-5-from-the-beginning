9679706a6a2be49890f698d133226b1059cbfae2
Denys Dvornyi <ddvornyi@geeksforless.net>
2019-06-14 17:28:20 +0300

    finished lesson #67 (started lesson #68 "Spring REST - CRUD Database Project - Delete Customer")

--- spring-crm-rest-demo/src/main/java/com/deno4ka/spring/crm/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/deno4ka/spring/crm/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +39,12 @@ public class CustomerRestController {
 		// also just in case the pass an id in JSON ... set id to 0
 		// this is force a save of new item ... instead of update
 		customer.setId(0); // if id is 0, then DAO will "INSERT" new customer
+		customerService.saveCustomer(customer);
+		return customer;
+	}
+
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
 		customerService.saveCustomer(customer);
 		return customer;
 	}
