f570a89d284637c08a95166253a1aee984a26b4d
Denys Dvornyi <ddvornyi@geeksforless.net>
2019-06-18 18:08:05 +0300

    finished lesson #70 (started lesson #71 "Spring Boot - Spring Boot Dev Tools and Spring Boot Actuator")

---
+++ spring-boot-demo/src/main/java/com/deno4ka/springboot/demo/SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.deno4ka.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = { "com.test.hello.world" })
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ spring-boot-demo/src/main/java/com/test/hello/world/TestController.java
@@ -0,0 +1,10 @@
+package com.test.hello.world;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+
+}
