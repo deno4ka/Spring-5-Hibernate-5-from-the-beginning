1074b22b8f248386c0cd0729965375b8b6b18fe8
Denys Dvornyi <ddvornyi@geeksforless.net>
2019-06-24 18:47:09 +0300

    finished lesson #77 (started lesson #78 "Spring Boot - Thymeleaf")

--- spring-boot-demo/src/main/java/com/deno4ka/springboot/demo/dao/EmployeeRepository.java
+++ spring-boot-demo/src/main/java/com/deno4ka/springboot/demo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.deno4ka.springboot.demo.dao;
 
 import com.deno4ka.springboot.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members") // if you need different path than default - entityName + 's' ("employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
--- spring-boot-demo/src/main/java/com/deno4ka/springboot/demo/rest/EmployeeController.java
+++ spring-boot-demo/src/main/java/com/deno4ka/springboot/demo/rest/EmployeeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController()
+//@RestController
 @RequestMapping("/api")
 public class EmployeeController {
 
